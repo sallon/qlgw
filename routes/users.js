a0e18832b665fda1b2160378afc6dde62465c69a
sallon <531601015@qq.com>
2016-12-01 14:44:50 +0800

    up
    up

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var userDao = require("../dao/userDao")
-
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('admin/login',{title:"登录企力官网控制台"});
+  res.send("您打开了未知世界！");
 });
 
-// 查询用户
-router.get('/findUser', function(req, res, next) {
-  console.log("test success")
-  userDao.find(req, res, next);
-  // res.send("sallon")
-});
-router.get('/findAll', function(req, res, next) {
-  console.log("test success")
-  userDao.findAll(req, res, next);
-  // res.send("sallon")
-});
-router.get('/index',function(req,res,next){
-  res.render('admin/index',{title:"官网管理"})
-})
+// var userDao = require("../dao/userDao")
+//
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.render('admin/login',{title:"登录企力官网控制台"});
+// });
+//
+// // 查询用户
+// router.get('/findUser', function(req, res, next) {
+//   console.log("test success")
+//   userDao.find(req, res, next);
+//   // res.send("sallon")
+// });
+// router.get('/findAll', function(req, res, next) {
+//   console.log("test success")
+//   userDao.findAll(req, res, next);
+//   // res.send("sallon")
+// });
+// router.get('/index',function(req,res,next){
+//   res.render('admin/index',{title:"官网管理"})
+// })
 
 module.exports = router;
