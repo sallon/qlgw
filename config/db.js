c8c10e831c5110d577ca7207f7c9f18a960afb94
sallon <531601015@qq.com>
2016-12-08 09:43:59 +0800

    sss
    ssss

--- config/db.js
+++ config/db.js
@@ -4,10 +4,17 @@
 // MySQL数据库联接配置
 module.exports = {
     mysql: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database:'keypowerplus', // 前面建的user表位于这个数据库中
+        host: 'rdsa6brqauzveai.mysql.rds.aliyuncs.com',
+        user: 'newlearn',
+        password: 'Shiyou123456',
+        database:'keypowerplus_test',
         port: 3306
     }
+    // mysql: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'root',
+    //     database:'keypowerplus', // 前面建的user表位于这个数据库中
+    //     port: 3306
+    // }
 };
